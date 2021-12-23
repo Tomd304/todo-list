9eb055d73d80c357cd47cda38e01928bcbbec12e
Tomd304 <tomd304@live.co.uk>
2021-12-23 16:47:57 +0000

    JS functions created to dynamically generate tables of projects and tasks

--- src/index.js
+++ src/index.js
@@ -25,13 +25,14 @@ function createProject(name, description) {
         toDoList.push(createToDo(title, desc, dueDate, priority))
     }
 
-    return {name, description, toDoList, addTaskToProject}
+    return {name, description, toDoList, addTask}
 }
 
 function saveProject(name, description) {
     console.log(name) 
     console.log(description)   
     projectList[name] = createProject(name, description)
+    createTable(projectList[name])
 }
 
 function printProjects() {
@@ -41,6 +42,54 @@ function printProjects() {
     })
 }
 
+function createTable(project) {
+    let pane = document.querySelector('.right-pane')
+    let table = createTableHeader(project.name)
+    project.toDoList.forEach((task) => {
+        table.appendChild(createTableRow(task))
+    })
+    pane.appendChild(table)
+}
+
+function createTableHeader(projectName) {
+    let table = document.createElement('table')
+    let row = document.createElement('tr')
+    let hTitle = document.createElement('th')
+    hTitle.textContent = 'Task'
+    let hDesc = document.createElement('th')
+    hDesc.textContent = 'Description'
+    let hDueDate = document.createElement('th')
+    hDueDate.textContent = 'Due Date'
+    let hPriority = document.createElement('th')
+    hPriority.textContent = 'Priority'
+    row.append(hTitle, hDesc, hDueDate, hPriority)
+    let heading = document.createElement('h2')
+    heading.textContent = projectName
+    table.append(heading, row)
+    return table
+}
+
+function createTableRow(task) {
+    let row = document.createElement('tr')
+    let title = document.createElement('td')
+    title.textContent = task.title
+    let desc = document.createElement('td')
+    desc.textContent = task.desc
+    let dueDate = document.createElement('td')
+    dueDate.textContent = task.dueDate
+    let priority = document.createElement('td')
+    priority.textContent = task.priority
+    row.append(title, desc, dueDate, priority)
+    return row
+
+}
+
+
+
+
+
+
+
 saveProject('Default Project', 'Default Description')
 saveProject('Uninmportant Items', 'Uninmportant Description')
 projectList["Default Project"].addTask('Read Email', 'Read all emails from today', '11/12/2021', 1)
@@ -48,6 +97,9 @@ projectList["Default Project"].addTask('Wash Car', 'Wash & wax car', '23/12/2021
 projectList["Uninmportant Items"].addTask('Celebrate Christmas', 'Enjoy!!!', '25/12/2021', 1)
 initCreateBtns()
 initCloseBtns() 
+
+createTable(projectList["Default Project"])
+createTable(projectList["Uninmportant Items"])
 console.log(projectList)
 
 export {saveProject, printProjects}

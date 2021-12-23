fcf9abf3f0a3a8916db0db0ea457155e12891641
Tomd304 <tomd304@live.co.uk>
2021-12-23 16:01:46 +0000

    Restructed project & task objects. Also added addtask to project function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import './styles.css'
 import {initCloseBtns, initCreateBtns} from './js_helpers/formButtons'
 
-let projectList = []
+let projectList = {}
 
 function createToDo(title, desc, dueDate, priority) {
     return {title, desc, dueDate, priority}
 }
+
 
 let createTaskBtn = document.querySelector('#create-task')
 createTaskBtn.addEventListener('click', (e) => {
@@ -18,17 +19,20 @@ createProjectBtn.addEventListener('click', (e) => {
 });
 
 function createProject(name, description) {
-    return {name,
-            description}
+    let toDoList = []
+
+    function addTask(title, desc, dueDate, priority) {
+        toDoList.push(createToDo(title, desc, dueDate, priority))
+    }
+
+    return {name, description, toDoList, addTaskToProject}
 }
 
 function saveProject(name, description) {
     console.log(name) 
     console.log(description)   
-    projectList.push(createProject(name, description))
+    projectList[name] = createProject(name, description)
 }
-
-
 
 function printProjects() {
     console.log(projectList)
@@ -37,8 +41,13 @@ function printProjects() {
     })
 }
 
-
+saveProject('Default Project', 'Default Description')
+saveProject('Uninmportant Items', 'Uninmportant Description')
+projectList["Default Project"].addTask('Read Email', 'Read all emails from today', '11/12/2021', 1)
+projectList["Default Project"].addTask('Wash Car', 'Wash & wax car', '23/12/2021', 2)
+projectList["Uninmportant Items"].addTask('Celebrate Christmas', 'Enjoy!!!', '25/12/2021', 1)
 initCreateBtns()
 initCloseBtns() 
+console.log(projectList)
 
 export {saveProject, printProjects}
